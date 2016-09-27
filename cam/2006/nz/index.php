56106aec76aa56d29812f53de5a63544ccfe077d
Paul Bausch <pbausch@gmail.com>
2016-09-27 16:17:13 -0700

    Get old image galleries working

---
+++ cam/2006/nz/index.php
@@ -0,0 +1,18 @@
+<?php
+require("onfocus-ini.inc");
+require(ROOT_DIR . "/lib/gallery.php");
+$pageTitle = "New Zealand 2016 Gallery";
+$pageNum = 1;
+$isDateArchive = 0;
+$dir = getcwd() . "/";
+require(ROOT_DIR . "header.php");
+print gallery($dir,$pageTitle);
+print <<<END
+<div id="footer">
+	<div class="navigation">
+		<a href="/">Home</a>
+	</div>
+</div>
+END;
+require(ROOT_DIR . "footer.php"); 
+?>
---
+++ lib/gallery.php
@@ -0,0 +1,26 @@
+<?php
+function gallery($dir, $title) {
+	$out = "<h2 class=\"archive-title\">$title</h2>\n";
+	$out .= "<div class=\"post archive\" style=\"margin-top:25px;\">\n";
+	$out .= "<div id=\"gallery\" class=\"post-text\" style=\"text-align:center;\">\n";
+
+	$images = glob($dir."*.jpg");
+	foreach($images as $image) {
+		list($width, $height, $type, $attr) = getimagesize($image);
+		if ($width > 640) {
+			$width = 640;
+		}
+		$out .= "<div class=\"galleryImage\" style=\"width:" . $width . "px;\">";
+		$out .= '<img data-src="'.basename($image).'" src="'.basename($image).'" width="'.$width.'" height="'.$height.'"/><br />' . "\n";
+		// Check for caption
+		if (file_exists(basename($image).".xml")) {
+			$xml = simplexml_load_file(basename($image).".xml");
+			$out .= '<div class="galleryCaption">' . $xml->caption . '</div>';
+		}
+		$out .=  "</div>";
+	}
+
+	$out .=  "</div></div></div>";
+	return $out;
+}
+?>
