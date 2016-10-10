bc809c8284b64cfbb8f588e64f3c6c624e694f43
Paul Bausch <pbausch@gmail.com>
2016-10-10 14:33:14 -0700

    Use on-the-fly image resizing for gallery thumbnails

--- archive.php
+++ archive.php
@@ -75,6 +75,7 @@ if (!$result = @ mysql_query ($query, $connection))
 				$galleryThumbURL = $tp[3];
 				$galleryThumbURL = str_replace("http://","https://",$galleryThumbURL);
 				$galleryThumbURL = str_replace("thumbs/","",$galleryThumbURL);
+				$galleryThumbURL = str_replace(".com/",".com/img/200x130/",$galleryThumbURL);
 				$galleryTotalPhotos = $tp[4];
 				print '<div class="galleryThumb"><a href="https://www.onfocus.com'.$galleryURL.'" title="'. $galleryName .' ('. date('Y', strtotime($galleryDTM)) .') / '. $galleryTotalPhotos .' photos"><img data-src="'.$galleryThumbURL.'" src="'.$galleryThumbURL.'" width="200" height="130"/></a></div>';
 			} ?>
