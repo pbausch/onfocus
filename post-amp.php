8a319d00e1eddffa9fcc48d0753bae43065d00dd
Paul Bausch <pbausch@gmail.com>
2016-10-18 22:38:52 -0700

    more schema updates

--- post-amp.php
+++ post-amp.php
@@ -154,7 +154,11 @@ $css = str_replace("!important","",$css);
         "@type": "BlogPosting",
         "headline": "<?php print $title ?>",
         "datePublished": "<?php print $thisAmpDate ?>",
-		"author": "Paul Bausch"
+		"author": "Paul Bausch",
+		"publisher": "Paul Bausch",
+        "image": [
+          "<?php print $firstImageUrl ?>"
+        ]
       }
     </script>
     <style amp-boilerplate>body{-webkit-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-moz-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-ms-animation:-amp-start 8s steps(1,end) 0s 1 normal both;animation:-amp-start 8s steps(1,end) 0s 1 normal both}@-webkit-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-moz-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-ms-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-o-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}</style><noscript><style amp-boilerplate>body{-webkit-animation:none;-moz-animation:none;-ms-animation:none;animation:none}</style></noscript>
