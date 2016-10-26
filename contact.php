2828b81b85fdd677a307396d58e4f6d37f6fdea6
Paul Bausch <pbausch@gmail.com>
2016-10-26 09:14:15 -0700

    Make the contact form responsive

--- contact.php
+++ contact.php
@@ -36,14 +36,33 @@ require("header.php");
 	<div class="post" style="margin-top:18px;">
 		<div class="post-text">This handy form will send a message directly to my inbox.
 		<br /><br />
-		<table border="0" cellspacing="5">
-		<form action="/contact" method="post" name="frmComment" onSubmit="return checkForm(this);">
-			<tr><td align="right" valign="middle"><label for="txtName"><nobr>Your Name&nbsp;</nobr></label></td><td><input id="txtName" name="txtName" type="text" size="35" maxlength="25" style="width:250px;background-color:#eee;padding:3px 2px;" onfocus="this.style.backgroundColor='#fff';" onblur="this.style.backgroundColor='#eee';" required="required" autofocus="autofocus"/></td></tr>
-			<tr><td align="right" valign="middle"><label for="txtEmail"><nobr>Your Email&nbsp;</nobr></label></td><td><input id="txtEmail" name="txtEmail" type="text" size="35" maxlength="100" style="width:250px;background-color:#eee;padding:3px 2px;" onfocus="this.style.backgroundColor='#fff';" onblur="this.style.backgroundColor='#eee';" type="email" required="required"/></td></tr>
-			<tr><td colspan="2" height="15">&nbsp;</td></tr>
-			<tr><td align="right" valign="middle"><label for="txtSubject"><nobr>Subject&nbsp;</nobr></label></td><td><input id="txtSubject" name="txtSubject" type="text" size="35" maxlength="100" style="width:250px;background-color:#eee;padding:3px 2px;" onfocus="this.style.backgroundColor='#fff';" onblur="this.style.backgroundColor='#eee';"/></td></tr>
-			<tr><td align="right" valign="top"><label for="txaBody"><nobr>Comments&nbsp;</nobr></label></td><td><textarea cols="40" rows="14" id="txaBody" name="txaBody" style="width:400px;background-color:#eee;padding:3px 2px;" onfocus="this.style.backgroundColor='#fff';" onblur="this.style.backgroundColor='#eee';"></textarea></td></tr>
-			<tr><td>&nbsp;</td><td><input class="btn" style="margin-top:10px;" type="submit" value="send message"/></td></tr>
+		<form action="/contact" method="post" name="frmComment" onSubmit="return checkForm(this);" id="cform">
+		<div class="formRow">
+			<label for="txtName"><nobr>Your Name&nbsp;</nobr></label>
+			<div class="formElement">
+				<input id="txtName" name="txtName" type="text" size="35" maxlength="25" style="width:250px;" onfocus="this.style.backgroundColor='#fff';" onblur="this.style.backgroundColor='#eee';" required="required" autofocus="autofocus" aria-required="true"/>
+			</div>
+		</div>
+		<div class="formRow">
+			<label for="txtEmail"><nobr>Your Email Address&nbsp;</nobr></label>
+			<div class="formElement">
+				<input id="txtEmail" name="txtEmail" type="text" size="35" maxlength="100" style="width:250px;" onfocus="this.style.backgroundColor='#fff';" onblur="this.style.backgroundColor='#eee';" type="email" required="required" aria-required="true"/>
+			</div>
+		</div>
+		<div class="formRow">
+			<label for="txtSubject"><nobr>Subject&nbsp;</nobr></label>
+			<div class="formElement">
+				<input id="txtSubject" name="txtSubject" type="text" size="35" maxlength="100" style="width:250px;" onfocus="this.style.backgroundColor='#fff';" onblur="this.style.backgroundColor='#eee';" aria-required="true"/>
+			</div>
+		</div>
+		<div class="formRow">
+			<label for="txaBody"><nobr>Message&nbsp;</nobr></label>
+			<div class="formElement">
+				<textarea cols="40" rows="14" id="txaBody" name="txaBody" style="width:400px;background-color:#eee;padding:3px 2px;" onfocus="this.style.backgroundColor='#fff';" onblur="this.style.backgroundColor='#eee';" aria-required="true"></textarea>
+			</div>
+		</div>
+			
+		<input class="btn" style="margin-top:10px;" type="submit" value="send message"/></td></tr>
 		</form>
 		</table>
 		</div>
