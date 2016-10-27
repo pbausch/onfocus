56f80b72087447f00e834f2400a8cf6a1439c3cc
Paul Bausch <pbausch@gmail.com>
2016-10-27 08:06:59 -0700

    Legacy snapgallery note fix

--- snapgallery.php
+++ snapgallery.php
@@ -2,11 +2,11 @@
 require("onfocus-ini.inc");
 $pageNum = 1;
 $isDateArchive = 0;
-$pagetitle = "SnapGallery is Gone | onfocus"
+$pagetitle = "SnapGallery is Gone | onfocus";
 require("header.php");
 ?><h2>SnapGallery No More</h2>
 	<div class="post" style="margin-top:18px;">
-		<div class="post-text">Back in the day I used to host a script at this location that helped DIY web publishers assemble a gallery of pictures for their website. It's gone now.
+		<div class="post-text">Years ago I used to host a script at this location that helped DIY web publishers assemble a gallery of pictures for their website. It's gone now.
 		<br /><br />
 		These days there are lots of great web applications to help you share pictures. <a href="http://www.flickr.com/">Flickr</a> is my favorite.
 		</div>
