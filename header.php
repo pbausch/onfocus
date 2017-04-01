d07fce25d766f1de75aad74e6c8863b089fcd5f5
Paul Bausch <pbausch@gmail.com>
2017-04-01 09:27:42 -0700

    remove background blend mode (boo mobile safari)

--- header.php
+++ header.php
@@ -24,15 +24,7 @@
 		#logo, .tagline, .selected, #bottom, #infobox .post-text a, .navigation a {color: #<?php print $logocolor ?> !important;}
 		html {
 			background: linear-gradient(to bottom, rgba(255,255,255,0.15) 0%, rgba(0,0,0,0.15) 100%), radial-gradient(at top center, rgba(255,255,255,0.40) 0%, rgba(0,0,0,0.40) 120%) #989898; 
-			background-blend-mode: multiply,multiply;
 			min-height: 100%;
-		}
-		body {
-	
-			
-		}
-		#footer, #bottom {
-			color: #<?php print $logocolor ?>;
 		}
 		.heartbox {
 			margin-top: 20px;
