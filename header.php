0c7dd30887fbfd8ae576d4107cb6198702841387
Paul Bausch <pbausch@gmail.com>
2017-10-21 12:21:37 -0700

    add background color for non linear-gradient browsers

--- header.php
+++ header.php
@@ -23,6 +23,7 @@
 	<style>
 		#logo, .tagline, .selected, #bottom, #infobox .post-text a, .navigation a {color: #<?php print $logocolor ?> !important;}
 		html {
+			background: #989898;
 			background: linear-gradient(to bottom, rgba(255,255,255,0.15) 0%, rgba(0,0,0,0.15) 100%), radial-gradient(at top center, rgba(255,255,255,0.40) 0%, rgba(0,0,0,0.40) 120%) #989898; 
 			min-height: 100%;
 		}
