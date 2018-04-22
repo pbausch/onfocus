ec0c77d0cb8031025cedc18742addc44a809167d
Paul Bausch <pbausch@gmail.com>
2018-04-22 14:32:48 -0700

    nix script type attributes

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <!-- <div id="bottom"><div id="copyright">\m/ &gt;_&lt; \m/</div></div> -->
-<?php if (isset($thisToken)) { if ($thisToken != '') { ?><script type="text/javascript" language="javascript">var token = '<?php print $thisToken; ?>';</script><?php }} ?>
-<script type="text/javascript" src="<?php print LIVE_SCRIPT ?>"></script>
+<?php if (isset($thisToken)) { if ($thisToken != '') { ?><script>var token = '<?php print $thisToken; ?>';</script><?php }} ?>
+<script src="<?php print LIVE_SCRIPT ?>"></script>
 <?php if (isset($pageFooterAddition)) { print $pageFooterAddition; } ?>
 <?php //include_once("lib/analyticstracking.php") ?>
 </body>
