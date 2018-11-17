3c116aaedb9534fe474f2d39e15411fa7022db64
Paul Bausch <pbausch@gmail.com>
2018-11-16 21:15:16 -0800

    add resizePhoto function

--- lib/site-functions.php
+++ lib/site-functions.php
@@ -26,4 +26,60 @@ function truncate_to_x_words($text, $number_of_words) {
    return $excerpt;
 }
 
+function resizePhoto($original,$width,$height,$destination,$crop) {
+	if ($originalImage  = @imagecreatefromjpeg($original)) {
+		$originalWidth  = imagesx($originalImage);
+		$originalHeight = imagesy($originalImage);
+		
+		$newImage  = imagecreatetruecolor($width, $height);
+		
+		//if we're creating a square thumb, remove 5 pixels around the edges
+		if ($width == THUMB_MAX_S && $height == THUMB_MAX_S) {
+			$cropWidth = ($originalWidth-10);
+			$cropHeight = ($originalHeight-10);
+			$cropImage  = imagecreatetruecolor($cropWidth, $cropHeight);
+
+			//initial crop to remove border
+			imagecopyresampled($cropImage, $originalImage, 0, 0, 5, 5, $cropWidth, $cropHeight, $cropWidth, $cropHeight);
+			
+			$originalWidth = $cropWidth;
+			$originalHeight = $cropHeight;
+		}
+		
+		if ($crop) {
+			if ($originalWidth > $originalHeight) {
+			   $offsetWidth = ($originalWidth-$originalHeight)/2;
+			   $offsetHeight = 0;
+			   $originalWidth = $originalHeight;
+			} elseif ($originalHeight > $originalWidth) {
+			   $offsetWidth = 0;
+			   $offsetHeight = ($originalHeight-$originalWidth)/2;
+			   $originalHeight = $originalWidth;
+			} else {
+			   $offsetWidth = 0;
+			   $offsetHeight = 0;
+			}
+		  	imagecopyresampled($newImage, $cropImage, 0, 0, $offsetWidth, $offsetHeight, $width, $height, $originalWidth, $originalHeight);
+		}
+		else {
+		  	imagecopyresampled($newImage, $originalImage, 0, 0, 0, 0, $width, $height, $originalWidth, $originalHeight);
+		}
+
+		//Create the image file
+		touch($destination);
+		imagejpeg($newImage, $destination, PHOTO_QUALITY);
+		chmod($destination, 0644);
+		imagedestroy($newImage);
+		imagedestroy($originalImage);
+		if(isset($cropImage)) {
+			imagedestroy($cropImage);
+		}
+		return true;
+		
+	}
+	else {
+		print "Couldn't load file: $original";
+		return false;
+	}
+}
 ?>
