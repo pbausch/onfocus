98251fcaddfcd3dcbd42556dc85e649c20e74436
Paul Bausch <pbausch@gmail.com>
2018-11-23 14:03:43 -0800

    add rain emoji, remove turkey emoji

--- index.php
+++ index.php
@@ -90,7 +90,7 @@ else {
 ?>
 
 <div class="welcome other"><div class="post-text">
-	<span class="emoji-outer emoji-sizer"><span class="emoji-inner emoji1f983" title="turkey"></span></span> Welcome to onfocus&mdash;a weblog by <a href="https://www.onfocus.com/about">Paul Bausch</a> where I post recommended links, my photos, and occasional thoughts. <a href="http://xml.onfocus.com/posts">Subscribe here</a> if you like RSS. <span class="emoji-outer emoji-sizer"><span class="emoji-inner emoji1f342" title="fallen_leaf"></span></span>
+	<span class="emoji-outer emoji-sizer"><span class="emoji-inner emoji1f327" title="rain_cloud"></span></span> Welcome to onfocus&mdash;a weblog by <a href="https://www.onfocus.com/about">Paul Bausch</a> where I post recommended links, my photos, and occasional thoughts. <a href="http://xml.onfocus.com/posts">Subscribe here</a> if you like RSS. <span class="emoji-outer emoji-sizer"><span class="emoji-inner emoji1f342" title="fallen_leaf"></span></span>
 </div></div>
 
 <?php
