861a714eee1721b5c3f2e3cd7a1c6d408d2e7c59
Paul Bausch <pbausch@gmail.com>
2018-12-11 20:18:46 -0800

    only show welcome message on Page 1

--- index.php
+++ index.php
@@ -86,6 +86,7 @@ if ($isDateArchive) {
 	$query = "SELECT post_id, DateCreated, title, body, (SELECT count(comment_id) FROM comments WHERE post_id = items.post_id AND hide = 0 AND trackback = 0) AS comment_count, comments_on, item_type_id, url_slug FROM items WHERE Year(DateCreated) = $thisArchiveYear AND Month(DateCreated) = $thisArchiveMonth AND hide = 0 ORDER BY DateCreated DESC";
 }
 else {
+	if ($pageNum == 1) {
 	// welcome message for the homepage
 ?>
 
@@ -94,6 +95,7 @@ else {
 </div></div>
 
 <?php
+	}
 	//print "<h2><span class=\"dmd\">&#9670;</span>&nbsp;&nbsp;a weblog by pb&nbsp;&nbsp;<span class=\"dmd\">&#9670;</span></h2>";
 	$query = "SELECT post_id, DateCreated, title, body, (SELECT count(comment_id) FROM comments WHERE post_id = items.post_id AND hide = 0 AND trackback = 0) AS comment_count, comments_on, item_type_id, url_slug FROM items WHERE hide = 0 ORDER BY DateCreated DESC LIMIT $offset, $rowsPerPage";
 }
