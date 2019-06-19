c15fdad44d19244cf432c9ecb606080bc3e8ef6d
Paul Bausch <pbausch@gmail.com>
2019-06-18 19:29:39 -0700

    update jquery version

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <!-- <div id="bottom"><div id="copyright">\m/ &gt;_&lt; \m/</div></div> -->
 <?php if (isset($thisToken)) { if ($thisToken != '') { ?><script>var token = '<?php print $thisToken; ?>';</script><?php }} ?>
-<script src="//code.jquery.com/jquery-3.2.1.min.js"></script>
+<script src="//code.jquery.com/jquery-3.4.1.min.js"></script>
 <script src="<?php print LIVE_SCRIPT ?>"></script>
 <script src="/js/jquery.fancybox.min.js"></script>
 <script>
