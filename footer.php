7e0fbf53dfcf48989bee01896664d403c33ab4f3
Paul Bausch <pbausch@gmail.com>
2021-03-10 18:42:17 -0800

    add owa tracking javascript

--- footer.php
+++ footer.php
@@ -4,6 +4,22 @@
 <script src="<?php print LIVE_SCRIPT ?>"></script>
 <script src="/js/jquery.fancybox.min.js"></script>
 <?php if (isset($pageFooterAddition)) { print $pageFooterAddition; } ?>
-<?php echo $tag_code; ?>
+<!-- Start Open Web Analytics Tracker -->
+<script type="text/javascript">
+//<![CDATA[
+var owa_baseUrl = 'https://www.onfocus.com/owa/';
+var owa_cmds = owa_cmds || [];
+owa_cmds.push(['setSiteId', '0eb8c5ed7e29d6444ee1b29be68cfa81']);
+owa_cmds.push(['trackClicks']);
+
+(function() {
+    var _owa = document.createElement('script'); _owa.type = 'text/javascript'; _owa.async = true;
+    owa_baseUrl = ('https:' == document.location.protocol ? window.owa_baseSecUrl || owa_baseUrl.replace(/http:/, 'https:') : owa_baseUrl );
+    _owa.src = owa_baseUrl + 'modules/base/js/owa.tracker-combined-min.js';
+    var _owa_s = document.getElementsByTagName('script')[0]; _owa_s.parentNode.insertBefore(_owa, _owa_s);
+}());
+//]]>
+</script>
+<!-- End Open Web Analytics Code -->
 </body>
 </html>
